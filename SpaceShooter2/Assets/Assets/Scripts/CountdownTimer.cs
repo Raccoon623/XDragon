46c46cbea541329b574dc404e8c103634f0e94a1
Raccoon623 <113412236+Raccoon623@users.noreply.github.com>
2023-04-25 08:08:04 +0300

    25.4.2023
    * Added countdown timer to UI
    * Set up some Enemy Wave settings
    * Created UFOBoss movement

---
+++ SpaceShooter2/Assets/Assets/Scripts/CountdownTimer.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using TMPro;
+
+public class CountdownTimer : MonoBehaviour
+{
+    public TMP_Text timeText;
+    public float countdownTime = 10f;
+
+    private float currentTime = 0f;
+
+    void Start()
+    {
+        currentTime = countdownTime;
+    }
+
+    void Update()
+    {
+        currentTime -= Time.deltaTime;
+        timeText.text = Mathf.Round(currentTime).ToString();
+
+        if (currentTime <= 0f)
+        {
+            currentTime = 0f;
+            timeText.text = "Time's up!";
+            //Do whatever you need to do when the timer reaches 0
+        }
+    }
+}
